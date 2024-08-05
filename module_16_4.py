9361577d7a601cf2c3402cd71c5df611e5f2c510
ArDa0226 <161866557+ArDa0226@users.noreply.github.com>
2024-08-05 11:11:59 +0500

    DZ_pydantic

--- module_16_4.py
+++ module_16_4.py
@@ -15,24 +15,28 @@ async def get_all_users() -> List[User]:
     return users
 
 @app.post('/user/{username}/{age}')
-async def create_user(user: User) -> str:
-    user.id = len(users)
+async def create_user(user: User) -> User:
+    user.id = len(users) + 1
     users.append(user)
-    return 'User created!'
+    return user
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: int, user_name: str = Body(), user_age: int = Body()) -> str:
+async def update_user(user_id: int, username: str, age: int) -> User:
     try:
-        edit_user = users[user_id]
-        edit_user.username = user_name
-        edit_user.age = user_age
-
+        edit_user = users[user_id - 1]
+        edit_user.username = username
+        edit_user.age = age
     except IndexError:
-        raise HTTPException(status_code=404, detail='User not found.')
+        raise HTTPException(status_code=404, detail='User was not found.')
+    return edit_user
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id: int) -> str:
+async def delete_user(user_id: int) -> User:
     try:
-        users.pop(user_id)
+        del_user = users.pop(user_id)
     except IndexError:
-        raise HTTPException(status_code=404, detail='Message not found')
+        raise HTTPException(status_code=404, detail='User was not found')
+    return del_user
+
+
+
