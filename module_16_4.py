e0f36ed47e3313381329e69366d5810c97249e27
ArDa0226 <161866557+ArDa0226@users.noreply.github.com>
2024-08-06 10:02:59 +0500

    ДЗ_по_теме:_Модели_данных_pydantic

--- module_16_4.py
+++ module_16_4.py
@@ -14,29 +14,29 @@ class User(BaseModel):
 async def get_all_users() -> List[User]:
     return users
 
-@app.post('/user/{username}/{age}')
+@app.post('/user/')
 async def create_user(user: User) -> User:
     user.id = len(users) + 1
     users.append(user)
     return user
 
-@app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: int, username: str, age: int) -> User:
+@app.put('/user/')
+async def update_user(user: User) -> User:
     try:
-        edit_user = users[user_id - 1]
-        edit_user.username = username
-        edit_user.age = age
+        edit_user = users[user.id - 1]
+        edit_user.username = user.username
+        edit_user.age = user.age
     except IndexError:
         raise HTTPException(status_code=404, detail='User was not found.')
     return edit_user
 
-@app.delete('/user/{user_id}')
-async def delete_user(user_id: int) -> User:
+@app.delete('/user/')
+async def delete_user(user: User) -> User:
     try:
-        del_user = users.pop(user_id - 1)
+        del_user = users.pop(user.id - 1)
     except IndexError:
-        raise HTTPException(status_code=404, detail='User was not found')
+        raise HTTPException(status_code=404, detail='User was not found.')
     return del_user
 
 
-
+print(users)
